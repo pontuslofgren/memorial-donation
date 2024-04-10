0d5dfb8904351361a2b52c6888dba081a185d9de
Pontus <pontus.loefgren@gmail.com>
2024-04-10 16:12:22 +0200

    Adds Tanstack Router

--- reactApp/vite.config.ts
+++ reactApp/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import mkcert from "vite-plugin-mkcert";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react(), mkcert(), TanStackRouterVite()],
   server: { https: true, port: 5173 },
 });
