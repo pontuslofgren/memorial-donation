6502df66fe84042c331150643cf55ad37aa21955
Pontus <pontus.loefgren@gmail.com>
2024-04-10 19:12:44 +0200

    Adds Automapper config for Donation and EmailTemplateRequest

--- backend/donationApi/Configs/AutomapperConfig.cs
+++ backend/donationApi/Configs/AutomapperConfig.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using donationApi.DTO;
 using donationApi.DTOs;
 using donationApi.Models;
 
@@ -10,5 +11,13 @@ public class AutomapperConfig : Profile
     {
         CreateMap<MemorialDonationRequest, MemorialDonation>().ReverseMap();
         CreateMap<MemorialDonationResponse, MemorialDonation>().ReverseMap();
+        CreateMap<MemorialDonation, EmailTemplateRequest>()
+            .ForMember(dest => dest.Params,
+                opt => opt.MapFrom(src => new EmailTemplateRequest.ParamsRecord { FNAME = src.DonorFirstName }))
+            .ForMember(dest => dest.To,
+                opt => opt.MapFrom(src => new List<EmailTemplateRequest.RecipientRecord>
+                    { new EmailTemplateRequest.RecipientRecord { Email = src.Email, Name = src.DonorFirstName } }))
+            .ForMember(dest => dest.Attachment, opt => opt.Ignore())
+            .ForMember(dest => dest.TemplateId, opt => opt.Ignore());
     }
 }
