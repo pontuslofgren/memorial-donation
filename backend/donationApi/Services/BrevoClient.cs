33806ba610ad7125ab7b3bdcc35bc96595a97d65
Pontus <pontus.loefgren@gmail.com>
2024-04-10 18:23:03 +0200

    Fixes error in constructor and adds temporary hard coded logic for SendTributeEmail

--- backend/donationApi/Services/BrevoClient.cs
+++ backend/donationApi/Services/BrevoClient.cs
@@ -8,15 +8,22 @@ public class BrevoClient: IBrevoClient
     private readonly RestClient _client;
     private readonly string _apiSecret;
 
-    public void BrevoClient(IConfiguration config)
+    public BrevoClient(IConfiguration config)
     {
         var options = new RestClientOptions("https://api.brevo.com/v3/smtp/email");
         _client = new RestClient(options);
-        _apiSecret = config["ApiKeys:BrevoApiKey"]
+        _apiSecret = config["ApiKeys:BrevoApiKey"];
     }
 
-    public Task SendTributeEmail(TributeEmail emailDetails)
+    public async Task SendTributeEmail(TributeEmail emailDetails)
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Sending email");
+        var request = new RestRequest("");
+        request.AddHeader("accept", "application/json");
+        request.AddHeader("api-key", _apiSecret);
+        // TODO: JsonBody from emailDetails
+        request.AddJsonBody("{\"sender\":{\"email\":\"postmaster@ullacarinstiftelse.se\",\"name\":\"Ulla-Carin Stiftelse\"},\"params\":{\"FIRSTNAME\":\"Pontus\",\"FNAME\":\"Ponta\"},\"attachment\":[{\"content\":\"b3JkZXIucGRm\",\"name\":\"myAttachment.png\"}],\"to\":[{\"email\":\"pontus@ullacarinstiftelse.se\",\"name\":\"Pontus\"}],\"tags\":[\"Test\"],\"templateId\":1}", false);
+        var response = await _client.PostAsync(request);
+        Console.WriteLine("{0}", response.Content);
     }
 }
