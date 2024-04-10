3f48dd18c139fb328aebe1b4f1992129db38f9e3
Pontus <pontus.loefgren@gmail.com>
2024-04-10 13:20:02 +0200

    Changes name

--- backend/donationApi/Data/DonationContext.cs
+++ backend/donationApi/Data/DonationContext.cs
@@ -12,5 +12,5 @@ using donationApi.Models;
         {
         }
 
-        public DbSet<donationApi.Models.Donation> Donation { get; set; } = default!;
+        public DbSet<MemorialDonation> Donation { get; set; } = default!;
     }
