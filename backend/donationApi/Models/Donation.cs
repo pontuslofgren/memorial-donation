f809b1cc346876a5b6d7e5929f21dfe59c4f3f85
Pontus <pontus.loefgren@gmail.com>
2024-04-10 13:20:17 +0200

    Adds props to MemorialDonation model

--- backend/donationApi/Models/Donation.cs
+++ backend/donationApi/Models/Donation.cs
@@ -2,8 +2,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace donationApi.Models;
 
-public class Donation
+public class MemorialDonation
 {
     [Key]
-    public int Id { get; set; }
+    public int Id { get; init; }
+    public Guid Guid { get; } = Guid.NewGuid();
+    public required string HonoreeName { get; set; }
+    public required string Message { get; set; }
+    public int Amount { get; set; }
+    public required string DonorFirstName { get; set; }
+    public required string DonorLastName { get; set; }
+    public required string Email { get; set; }
+    public bool HasSucceededPayment { get; set; } = false;
+    public DateTime CreatedAt { get; init; } = DateTime.Now;
 }
