00cca3da5b4241e1bd2634b8996f3015ea0e7892
Pontus <pontus.loefgren@gmail.com>
2024-04-10 11:08:56 +0200

    Fixes TS warnings

--- reactApp/src/memorialForm/MemorialForm.tsx
+++ reactApp/src/memorialForm/MemorialForm.tsx
@@ -1,33 +1,31 @@
 import { PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { StripeError } from "@stripe/stripe-js";
 import { useState } from "react";
 
 function MemorialForm() {
     const stripe = useStripe();
     const elements = useElements();
 
-    const [errorMessage, setErrorMessage] = useState();
-    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState<string>();
+    const [loading, setLoading] = useState<boolean>(false);
 
-    const handleError = (error) => {
+    const handleError = (error: StripeError) => {
         setLoading(false);
         setErrorMessage(error.message);
     }
 
-    const handleSubmit = async (event) => {
-        // We don't want to let default form submission happen here,
-        // which would refresh the page.
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
         if (!stripe) {
             // Stripe.js hasn't yet loaded.
-            // Make sure to disable form submission until Stripe.js has loaded.
             return;
         }
 
         setLoading(true);
 
         // Trigger form validation and wallet collection
-        const { error: submitError } = await elements.submit();
+        const { error: submitError } = await elements!.submit();
         if (submitError) {
             handleError(submitError);
             return;
@@ -42,7 +40,7 @@ function MemorialForm() {
 
         // Confirm the PaymentIntent using the details collected by the Payment Element
         const { error } = await stripe.confirmPayment({
-            elements,
+            elements: elements ? elements : undefined,
             clientSecret,
             confirmParams: {
                 // TODO: create success route and add return url
@@ -54,10 +52,6 @@ function MemorialForm() {
             // This point is only reached if there's an immediate error when
             // confirming the payment. Show the error to your customer (for example, payment details incomplete)
             handleError(error);
-        } else {
-            // Your customer is redirected to your `return_url`. For some payment
-            // methods like iDEAL, your customer is redirected to an intermediate
-            // site first to authorize the payment, then redirected to the `return_url`.
         }
     };
     return (
