5f924c78c6de5e72f41159cf5296d958d488a8ba
Pontus <pontus.loefgren@gmail.com>
2024-04-10 10:18:24 +0200

    Adds Stripe setup

--- reactApp/src/app/App.tsx
+++ reactApp/src/app/App.tsx
@@ -1,8 +1,23 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { StripeElementsOptions, loadStripe } from "@stripe/stripe-js"
+import MemorialForm from "../memorialForm/MemorialForm";
+
+const stripePromise = loadStripe('pk_test_4QHSdRjQiwkzokPPCiK33eOq');
+
 function App() {
+  // TODO: useState amount
+
+  const options: StripeElementsOptions = {
+    mode: 'payment',
+    currency: 'usd',
+    amount: 1099,
+  }
 
   return (
     <>
-      Hello world!
+      <Elements stripe={stripePromise} options={options}>
+        <MemorialForm />
+      </Elements>
     </>
   )
 }
