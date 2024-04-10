74949a47805aeeae323810a0172991eecb7eeda5
Pontus <pontus.loefgren@gmail.com>
2024-04-10 19:13:20 +0200

    Removes SenderRecord from EmailTemplateRequest (not needed when passing template ID)

--- backend/donationApi/DTOs/EmailTemplateRequest.cs
+++ backend/donationApi/DTOs/EmailTemplateRequest.cs
@@ -2,18 +2,11 @@ namespace donationApi.DTO;
 
 public record EmailTemplateRequest
 {
-    public required SenderRecord Sender { get; set; }
     public ParamsRecord? Params { get; set; }
     public required List<RecipientRecord> To { get; set; }
     public List<AttachmentRecord>? Attachment { get; set; }
     public int TemplateId { get; set; }
     
-    public record SenderRecord
-    {
-        public required string Name { get; set; }
-        public required string Email { get; set; }
-    }
-
     public record ParamsRecord
     {
         public required string FNAME { get; set; }
