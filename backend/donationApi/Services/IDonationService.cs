5317d44d5f965c962250985666580783539dc696
Pontus <pontus.loefgren@gmail.com>
2024-04-11 10:31:23 +0200

    Updates interface to consider implementation of HandlePaymentSuccess

--- backend/donationApi/Services/IDonationService.cs
+++ backend/donationApi/Services/IDonationService.cs
@@ -1,12 +1,13 @@
 using donationApi.DTOs;
+using donationApi.Models;
 using Stripe;
 
 namespace donationApi.Services;
 
 public interface IDonationService
 {
+    public Task HandlePaymentSuccess(string clientSecret);
     public Task CreateDonation(MemorialDonationRequest request, StripeClientSecretResponse clientSecret);
-    public Task SetDonationStatusToSucceeded(string clientSecret);
-    // public MemorialDonationResponse GetDonation(Guid guid);
-    public Task SendTributeEmail(string clientSecret);
+    public Task SetDonationStatusToSucceeded(MemorialDonation donation);
+    public Task SendTributeEmail(MemorialDonation donation);
 }
