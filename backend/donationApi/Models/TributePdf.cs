b21bcc7ddf6c8c2aa1635df74add144c04ccce85
Pontus <pontus.loefgren@gmail.com>
2024-04-11 10:32:19 +0200

    Changes Honoree property name to HonoreeName to match Donation model for automapper

--- backend/donationApi/Models/TributePdf.cs
+++ backend/donationApi/Models/TributePdf.cs
@@ -2,6 +2,6 @@ namespace donationApi.Models;
 
 public class TributePdf
 {
-    public required string Honoree { get; set; }
+    public required string HonoreeName { get; set; }
     public required string Message { get; set; }
 }
