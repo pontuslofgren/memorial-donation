9e577202e3c17b95283a677243758a1af308677e
Pontus <pontus.loefgren@gmail.com>
2024-04-12 08:39:34 +0200

    Adds Header

--- reactApp/src/AppProvider.tsx
+++ reactApp/src/AppProvider.tsx
@@ -2,6 +2,7 @@ import { createContext, useState } from "react";
 import { useAppState } from "./useAppState";
 import { Elements } from "@stripe/react-stripe-js";
 import { StripeElementsOptions, loadStripe } from "@stripe/stripe-js"
+import { Header } from "./components/Header";
 
 const stripePromise = loadStripe('pk_test_51OJg4MDGsFuWotrxIC3YGoqZV56afGxQIlgb7wrnQ9Er0Pi2Npedc2LNXWyNiD5m9aOmEvOtndtgABcegP9E93u500bcJBhLYM');
 
@@ -18,7 +19,8 @@ export function AppProvider({ children }) {
     return (
         <Elements stripe={stripePromise} options={options}>
             <AppStateContext.Provider value={value}>
-                <div className="px-3">
+                <Header />
+                <div className="px-3 max-w-screen-sm mx-auto">
                     {children}
                 </div>
             </AppStateContext.Provider>
