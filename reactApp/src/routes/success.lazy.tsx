4d467b3aa9bb15a9f1ee635fefb16de63ed5a2d3
Pontus <pontus.loefgren@gmail.com>
2024-04-12 10:32:57 +0200

    Adds confetti to success page

--- reactApp/src/routes/success.lazy.tsx
+++ reactApp/src/routes/success.lazy.tsx
@@ -1,9 +1,28 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import Confetti from 'react-confetti'
+import useWindowSize from 'react-use/lib/useWindowSize'
+
+
+
 
 export const Route = createLazyFileRoute('/success')({
     component: Success,
 })
 
 function Success() {
-    return <div className="p-2">Hello from Success!</div>
+    const { width, height } = useWindowSize()
+
+    return (
+        <>
+            <Confetti
+                width={width}
+                height={height}
+            />
+
+            <div className="my-6">
+                <h2 className="text-xl mb-3 font-semibold">Thank you for your donation!</h2>
+                <p>You will soon receive the eCard to your email inbox.</p>
+            </div>
+        </>
+    )
 }
